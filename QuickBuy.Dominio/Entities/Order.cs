db2cdfb4bdfc15bf15532a3e99524024c7657b12
Gustavo Marques <gus_sm_98@hotmail.com>
2020-01-14 09:49:20 -0300

    criação das entidades

---
+++ QuickBuy.Dominio/Entities/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int UserId { get; set; }
+        public DateTime DeliveryPrevisionTime { get; set; }
+        public string CEP { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public string FullAddress { get; set; }
+        public string AddressNumber { get; set; }
+
+        public ICollection<OrderItem> OrderItems { get; set; }
+    }
+}
---
+++ QuickBuy.Dominio/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entities
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public ICollection<Order> Orders { get; set; }
+    }
+}
